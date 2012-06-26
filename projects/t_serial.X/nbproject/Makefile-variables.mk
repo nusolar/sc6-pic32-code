88b80b8a3d5e534e458ddd5a0853a1e95f08e20c
Chris Yungmann <yungmann.chris@gmail.com>
2012-06-25 20:52:19 -0500

    t_serial.X: project created for testing serial on nu32

---
+++ projects/t_serial.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=t_serial.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/t_serial.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=tserial.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/tserial.x.tar
