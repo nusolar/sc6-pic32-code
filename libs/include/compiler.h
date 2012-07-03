0f241483a64f14f0c8034d9fd650c56501ee9830
Chris Yungmann <yungmann.chris@gmail.com>
2012-07-03 04:00:09 -0500

    compiler.h: new library for compiler-specific macros

---
+++ libs/include/compiler.h
@@ -0,0 +1,16 @@
+#ifndef NU_COMPILER_H
+#define NU_COMPILER_H
+
+#ifndef __DEBUG
+#define INLINE          inline
+#define ALWAYSINLINE    inline  __attribute__((always_inline))
+#else
+#define INLINE
+#define ALWAYSINLINE
+#endif
+
+#define NORETURN        __attribute__((noreturn))
+#define PURE            __attribute__((pure))
+#define UNUSED          __attribute__((unused))
+
+#endif
