04b70098b935f309626c46c9a3060d8549e217f5
Chris Yungmann <yungmann.chris@gmail.com>
2012-09-02 13:16:18 -0500

    can_dc.h, can_steering_wheel.h: rearrange can structs

--- libs/include/can_dc.h
+++ libs/include/can_dc.h
@@ -1,34 +1,37 @@
 #ifndef NU_CAN_DC_H
 #define NU_CAN_DC_H
 
+#include "utility.h"
+
 /* CAN for driver controls */
 
-union can_dc;
-union can_dc_rx;
-
-struct can_dc_rx_reverse {
-    int64_t reverseEnabled;
-};
-
-struct can_dc_rx_cruise {
-    int64_t cruiseEnabled;
-};
-
-struct can_dc_rx_cruise_velocity_current {
-    /*Units: m/s*/
-    float velocity;
-    /*Units: A*/
-    float current;
-};
-
-union can_dc_rx {
-    struct can_dc_rx_reverse                    reverse;
-    struct can_dc_rx_cruise                     cruise;
-    struct can_dc_rx_cruise_velocity_current    cruise_velocity_current;
-};
-
 union can_dc {
-    union can_dc_rx    rx;
+    union can_dc_rx {
+        struct can_dc_rx_horn {
+            int64_t enabled;
+        } horn;
+        struct can_dc_rx_signals {
+            uint32_t l;
+            uint32_t r;
+        } signals;
+        struct can_dc_rx_reverseEnable {
+            int64_t reverseEnabled;
+        } reverseEnable;
+        struct can_dc_rx_cruise {
+            int64_t cruiseEnabled;
+        } cruise;
+        struct can_dc_rx_cruise_velocity_current {
+            /*Units: m/s*/
+            float velocity;
+            /*Units: A*/
+            float current;
+        } cruise_velocity_current;
+    } rx;
+    union can_dc_tx {
+        
+    } tx;
 };
+
+STATIC_ASSERT(sizeof(union can_dc) <= 8, SIZE_MISMATCH);
 
 #endif
--- libs/include/can_steering_wheel.h
+++ libs/include/can_steering_wheel.h
@@ -12,6 +12,38 @@ union can_steering_wheel {
             unsigned    radio           :1;
             unsigned    yes             :1;
             unsigned    hazard          :1;
+            unsigned    cruise_en       :1;
+            unsigned    cruise_up       :1;
+            unsigned    maybe           :1;
+            unsigned    no              :1;
+            unsigned    horn            :1;
+            unsigned    cruise_mode     :1;
+            unsigned    cruise_down     :1;
+            unsigned    reserved        :20;
+        } lights __attribute__((packed));
+    } rx;
+    union can_steering_wheel_tx {
+        struct can_steering_wheel_tx_buttons {
+            unsigned    left            :1;
+            unsigned    right           :1;
+            unsigned    yes             :1;
+            unsigned    no              :1;
+            unsigned    maybe           :1;
+            unsigned    hazard          :1;
+            unsigned    horn            :1;
+            unsigned    cruise_en       :1;
+            unsigned    cruise_mode     :1;
+            unsigned    cruise_up       :1;
+            unsigned    cruise_down     :1;
+            unsigned    reserved        :21;
+        } buttons __attribute__((packed));
+        struct can_steering_wheel_tx_lights {
+            /* arranged left to right, top to bottom */
+            unsigned    left            :1;
+            unsigned    right           :1;
+            unsigned    radio           :1;
+            unsigned    yes             :1;
+            unsigned    hazard          :1;
             unsigned    cruiseEnable    :1;
             unsigned    cruiseUp        :1;
             unsigned    maybe           :1;
@@ -21,36 +53,12 @@ union can_steering_wheel {
             unsigned    cruiseDown      :1;
             unsigned    reserved        :20;
         } lights __attribute__((packed));
-    } rx;
-    union can_steering_wheel_tx {
-        struct can_steering_wheel_tx_cruise {
-            union {
-                float   speedMperS;
-                float   currentA;
-            };
-            enum cruiseMode {
-                CRUISE_NONE,
-                CRUISE_VELOCITY,
-                CRUISE_CURRENT,
-            } mode;
-        } cruise;
-        struct can_steering_wheel_tx_peripheral {
-            unsigned    l           :1;
-            unsigned    r           :1;
-            unsigned    horn        :1;
-            unsigned    reserved    :29;
-        } peripheral __attribute__((packed));
-        struct can_steering_wheel_tx_comm {
-            enum comm {
-                COMM_NONE,
-                COMM_YES,
-                COMM_NO,
-                COMM_MAYBE,
-            } comm;
-            unsigned reserved :32;
-        } comm;
     } tx;
 };
+
+STATIC_ASSERT(sizeof(struct can_steering_wheel_rx_lights) == 4, SIZE_MISMATCH);
+STATIC_ASSERT(sizeof(struct can_steering_wheel_tx_buttons) == 4, SIZE_MISMATCH);
+STATIC_ASSERT(sizeof(union can_steering_wheel) <= 8, SIZE_MISMATCH);
 
 #endif
 
