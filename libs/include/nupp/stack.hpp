754d12fe650c416fac35515c1e2c89f6bd9bfbfe
Alex Chandel <alexchandel@gmail.com>
2013-06-06 11:28:53 -0500

    new template class, for stack arrays, or "stacks"

---
+++ libs/include/nupp/stack.hpp
@@ -0,0 +1,23 @@
+#include "nu/compiler.h"
+
+#ifndef NUPP_STACK_HPP
+#define	NUPP_STACK_HPP
+
+namespace nu {
+	template <typename T, size_t N>
+	class stack {
+		T array[N];
+	public:
+		ALWAYSINLINE T& operator[] (const size_t index) {
+			return array[index];
+		}
+		ALWAYSINLINE operator void *() {
+			return (void *)array;
+		}
+		ALWAYSINLINE operator const void *() const {
+			return (const void *)array;
+		}
+	};
+}
+
+#endif	/* NUPP_STACK_HPP */
