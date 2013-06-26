eb0f39f53ce591b5c72b6f8b4ba36dfc3fea2a14
Alex Chandel <alexchandel@gmail.com>
2013-06-26 03:17:31 -0500

    Revert "Revert "Merge commit: fix timer overflows""
    This reverts commit e98e0f52e50d004627bd59f9ff08024da070f982.

--- libs/include/nupp/timer.hpp
+++ libs/include/nupp/timer.hpp
@@ -2,6 +2,7 @@
 #define NUPP_TIMER_HPP 1
 
 #include "nu/compiler.h"
+#include "nupp/param.hpp"
 extern "C" {
 #include "nu/timer.h"
 }
@@ -12,40 +13,40 @@ namespace nu {
 		/** Ticks happen every other clockcycle */
 		static ALWAYSINLINE void reset() {nu_timer_reset();}
 
-		template <u32 s>
-		static ALWAYSINLINE u32  s_to_ticks()		{return nu_s_to_ticks(s);}
-		static ALWAYSINLINE u32  s_to_ticks(u32 s)	{return nu_s_to_ticks(s);}
-		template <u32 ms>
-		static ALWAYSINLINE u32 ms_to_ticks()		{return nu_ms_to_ticks(ms);}
-		static ALWAYSINLINE u32 ms_to_ticks(u32 ms)	{return nu_ms_to_ticks(ms);}
-		template <u32 us>
-		static ALWAYSINLINE u32 us_to_ticks()		{return nu_us_to_ticks(us);}
-		static ALWAYSINLINE u32 us_to_ticks(u32 us)	{return nu_us_to_ticks(us);}
-		template <u32 ns>
-		static ALWAYSINLINE u32 ns_to_ticks()		{return nu_ns_to_ticks(ns);}
-		static ALWAYSINLINE u32 ns_to_ticks(u32 ns)	{return nu_ns_to_ticks(ns);}
+		template <u64 s>
+		static ALWAYSINLINE u64  s_to_ticks()		{return nu_s_to_ticks(s);}
+		static ALWAYSINLINE u64  s_to_ticks(u64 s)	{return nu_s_to_ticks(s);}
+		template <u64 ms>
+		static ALWAYSINLINE u64 ms_to_ticks()		{return nu_ms_to_ticks(ms);}
+		static ALWAYSINLINE u64 ms_to_ticks(u64 ms)	{return nu_ms_to_ticks(ms);}
+		template <u64 us>
+		static ALWAYSINLINE u64 us_to_ticks()		{return nu_us_to_ticks(us);}
+		static ALWAYSINLINE u64 us_to_ticks(u64 us)	{return nu_us_to_ticks(us);}
+		template <u64 ns>
+		static ALWAYSINLINE u64 ns_to_ticks()		{return nu_ns_to_ticks(ns);}
+		static ALWAYSINLINE u64 ns_to_ticks(u64 ns)	{return nu_ns_to_ticks(ns);}
 
-		static ALWAYSINLINE u32 s ()	{return nu_timer_s();}
+		static ALWAYSINLINE u64 s ()	{return (int64_t)(nu_timer_ticks()<<1)/NU_HZ;}
 		static ALWAYSINLINE u64 ms()	{return (int64_t)nu_timer_ticks()*2000/NU_HZ;}
-		static ALWAYSINLINE u32 us()	{return nu_timer_us();}
-		static ALWAYSINLINE u32 ns()	{return nu_timer_ns();}
-		static ALWAYSINLINE u32 ticks()	{return nu_timer_ticks();}
+		static ALWAYSINLINE u64 us()	{return (int64_t)(nu_timer_ticks()<<1)/NU_MHZ;}
+		static ALWAYSINLINE u64 ns()	{return (int64_t)(nu_timer_ticks()*2000)/NU_MHZ;}
+		static ALWAYSINLINE u64 ticks()	{return nu_timer_ticks();}
 
-		template <u32 s>
-		static ALWAYSINLINE void delay_s ()			{nu_delay_s(s);}
-		static ALWAYSINLINE void delay_s (u32 s)	{nu_delay_s(s);}
-		template <u32 ms>
-		static ALWAYSINLINE void delay_ms()			{nu_delay_ms(ms);}
-		static ALWAYSINLINE void delay_ms(u32 ms)	{nu_delay_ms(ms);}
-		template <u32 us>
-		static ALWAYSINLINE void delay_us()			{nu_delay_us(us);}
-		static ALWAYSINLINE void delay_us(u32 us)	{nu_delay_us(us);}
-		template <u32 ns>
-		static ALWAYSINLINE void delay_ns()			{nu_delay_ns(ns);}
-		static ALWAYSINLINE void delay_ns(u32 ns)	{nu_delay_ns(ns);}
-		template <u32 ticks>
+		template <u64 ticks>
 		static ALWAYSINLINE void delay_ticks()			{nu_delay_ticks(ticks);}
-		static ALWAYSINLINE void delay_ticks(u32 ticks)	{nu_delay_ticks(ticks);}
+		static ALWAYSINLINE void delay_ticks(u64 ticks)	{nu_delay_ticks(ticks);}
+		template <u64 s>
+		static ALWAYSINLINE void delay_s ()			{delay_ticks((tick_t)(s*param::Hz()/2));}
+		static ALWAYSINLINE void delay_s (u64 s)	{delay_ticks((tick_t)(s*param::Hz()/2));}
+		template <u64 ms>
+		static ALWAYSINLINE void delay_ms()			{delay_ticks((tick_t)(ms*param::Hz()/2000));}
+		static ALWAYSINLINE void delay_ms(u64 ms)	{delay_ticks((tick_t)(ms*param::Hz()/2000));}
+		template <u64 us>
+		static ALWAYSINLINE void delay_us()			{delay_ticks((tick_t)(us*param::MHz()/2));}
+		static ALWAYSINLINE void delay_us(u64 us)	{delay_ticks((tick_t)(us*param::MHz()/2));}
+		template <u64 ns>
+		static ALWAYSINLINE void delay_ns()			{delay_ticks((tick_t)(ns*param::MHz()/2000));}
+		static ALWAYSINLINE void delay_ns(u64 ns)	{delay_ticks((tick_t)(ns*param::MHz()/2000));}
 
 	}
 }
