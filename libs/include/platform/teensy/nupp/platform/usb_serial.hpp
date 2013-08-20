7b3e701636de2d71c47b636708bbe0bec5bdeda7
Alex Chandel <alexchandel@gmail.com>
2013-08-19 19:19:31 -0500

    experimental teensy usb-serial code

---
+++ libs/include/platform/teensy/nupp/platform/usb_serial.hpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   usb_serial.hpp
+ * Author: alex
+ *
+ * Created on July 7, 2013, 3:49 PM
+ */
+
+#ifndef NU_USB_SERIAL_HPP
+#define	NU_USB_SERIAL_HPP
+
+#include "nu/compiler.h"
+#include "nu/platform/usb_serial.h"
+
+namespace nu {
+	struct USBSerial {
+		USBSerial() {
+			usb_init();
+		}
+
+		/** returns -1 if nothing is available */
+		ALWAYSINLINE int16_t rx_char() {
+			if (usb_serial_available())
+				return usb_serial_getchar();
+			return -1;
+		}
+
+		ALWAYSINLINE void flush_input() {
+			usb_serial_flush_input();
+		}
+
+		ALWAYSINLINE int8_t putc(uint8_t c) {
+			return usb_serial_putchar_nowait(c);
+		}
+	};
+}
+
+#endif	/* NU_USB_SERIAL_HPP */
+
